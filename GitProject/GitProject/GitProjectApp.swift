2cce51ac3742ff6adda356e0454d6fbf4d230491
Shlyakhovenko Vladislav <homyak603@gmail.com>
2025-04-07 17:19:19 +0300

    запушил проект

---
+++ GitProject/GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Владислав Шляховенко on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
